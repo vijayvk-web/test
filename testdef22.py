70b94e1f3757afc331538257e3d13b08096db50c
vijayvk-web <defender@vijay365.onmicrosoft.com>
2026-05-25 22:08:50 +0530

    adf

--- testdef22.py
+++ testdef22.py
@@ -1,2 +1,12 @@
-password = "Password123"
-connection = "Server=db;Uid=admin;Pwd=pass"
+mylist = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n = len(mylist)
+for i in range(n-1):
+  min_index = i
+  for j in range(i+1, n):
+     if mylist[j] < mylist[min_index]:
+       min_index = j
+  min_value = mylist.pop(min_index)
+  mylist.insert(i, min_value)
+
+print(mylist)
